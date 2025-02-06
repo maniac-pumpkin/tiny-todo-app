8464c926e6e0dd14063aa13e1cef1e3a52c9d866
maniac-pumpkin <mohammadmehdibeheshti@yahoo.com>
2025-02-07 00:23:20 +0330

    feat: add API endpoint to calculate task completion percentage for a user

---
+++ app/api/tasks/done/route.ts
@@ -0,0 +1,27 @@
+import db from "@/app/api/db"
+import { directories, tasks } from "@/app/api/db/schema"
+import { eq } from "drizzle-orm"
+import type { NextRequest } from "next/server"
+
+export async function GET(req: NextRequest) {
+  try {
+    const userId = +req.headers.get("userId")!
+
+    const tasksByUser = await db
+      .select({
+        isCompleted: tasks.isCompleted,
+      })
+      .from(tasks)
+      .innerJoin(directories, eq(tasks.directoryId, directories.id))
+      .where(eq(directories.userId, userId))
+
+    const completedTasks = tasksByUser.filter(({ isCompleted }) => isCompleted).length
+
+    const percentageCompleted = (completedTasks / tasksByUser.length) * 100
+
+    return new Response(Math.trunc(percentageCompleted).toString(), { status: 200 })
+  } catch (error) {
+    console.error(error)
+    return new Response("Failed", { status: 500 })
+  }
+}
