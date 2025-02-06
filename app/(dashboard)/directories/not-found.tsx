a9738adf229523984567f19f11e52dd77e81a07a
maniac-pumpkin <mohammadmehdibeheshti@yahoo.com>
2025-02-07 00:23:05 +0330

    fix: update not found message for clarity in Directory component

--- app/(dashboard)/directories/not-found.tsx
+++ app/(dashboard)/directories/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div className="absolute left-2/4 top-2/4 flex -translate-x-2/4 -translate-y-2/4 items-center gap-x-2">
       <TriangleAlert />
-      <h3 className="text-lg font-semibold">Directory not found.</h3>
+      <h3 className="text-lg font-semibold">No content in this directory.</h3>
     </div>
   )
 }
