28836cb6573acb673f032637913ba1b685fc92a1
maniac-pumpkin <mohammadmehdibeheshti@yahoo.com>
2025-02-07 00:23:37 +0330

    feat: fetch and display dynamic task completion percentage in AppSidebarProgress component

--- components/app-sidebar/app-sidebar-progress.tsx
+++ components/app-sidebar/app-sidebar-progress.tsx
@@ -1,13 +1,17 @@
 import { Progress } from "@/components/ui/progress"
+import useFetchFn from "@/hooks/use-fetch-fn"
+import { taskGetCompleted } from "@/lib/client-api-services"
 
 function AppSidebarProgress() {
+  const { data } = useFetchFn(taskGetCompleted)
+
   return (
     <section className="mx-4">
       <div className="mb-2 flex justify-between">
         <span className="text-sm">Tasks done</span>
-        <span className="text-sm">20%</span>
+        <span className="text-sm">{data ?? 0}%</span>
       </div>
-      <Progress className="h-3" value={20} />
+      <Progress className="h-3" value={+data!} />
     </section>
   )
 }
