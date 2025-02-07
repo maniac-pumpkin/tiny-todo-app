d8efae54b668053011edade8c5a72a29f8a2aab6
maniac-pumpkin <mohammadmehdibeheshti@yahoo.com>
2025-02-07 13:55:06 +0330

    feat: refactor progress display to use zustand state management

--- components/app-sidebar/app-sidebar-progress.tsx
+++ components/app-sidebar/app-sidebar-progress.tsx
@@ -1,17 +1,16 @@
 import { Progress } from "@/components/ui/progress"
-import useFetchFn from "@/hooks/use-fetch-fn"
-import { taskGetCompleted } from "@/lib/client-api-services"
+import useTasksProgress from "@/hooks/use-tasks-progress"
 
 function AppSidebarProgress() {
-  const { data } = useFetchFn(taskGetCompleted)
+  const progress = useTasksProgress((state) => state.progress)
 
   return (
     <section className="mx-4">
       <div className="mb-2 flex justify-between">
         <span className="text-sm">Tasks done</span>
-        <span className="text-sm">{data ?? 0}%</span>
+        <span className="text-sm">{progress}%</span>
       </div>
-      <Progress className="h-3" value={+data!} />
+      <Progress className="h-3" value={progress} />
     </section>
   )
 }
