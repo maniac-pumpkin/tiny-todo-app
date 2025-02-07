aa07d88e37bdb8e8dc6c2a683f53e37660319f1d
maniac-pumpkin <mohammadmehdibeheshti@yahoo.com>
2025-02-07 13:55:27 +0330

    feat: integrate zustand for directory deletion refresh

--- components/app-sidebar/app-sidebar-dirs/dirs-delete.tsx
+++ components/app-sidebar/app-sidebar-dirs/dirs-delete.tsx
@@ -3,18 +3,20 @@
 import { directoryDelete } from "@/app/actions/directory-actions"
 import AlertDialog from "@/components/dialogs/alert-dialog"
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu"
+import useDirectories from "@/hooks/use-directories"
 import { toast } from "@/hooks/use-toast"
 import { Trash } from "lucide-react"
-import type { DirOpPropsType } from "./dirs-index"
 
-function DeleteDirectory({ id, fetchData }: DirOpPropsType) {
+function DeleteDirectory({ id }: { id: number }) {
+  const refresh = useDirectories((state) => state.refresh)
+
   const handleAction = async () => {
     if (await directoryDelete(id)) {
       toast({
         title: "Directory deleted",
         description: "The directory and all its tasks have been successfully deleted.",
       })
-      fetchData()
+      refresh()
     } else
       toast({
         title: "Deletion failed",
