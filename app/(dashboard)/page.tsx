0a381a19990bf94fd7b1a8f9e58c99ef79e2dac3
maniac-pumpkin <mohammadmehdibeheshti@yahoo.com>
2025-02-10 16:36:00 +0330

    refactor: enable task fetching in dashboard page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,8 @@
 import TaskCardWrapper from "@/components/task-card/task-card-wrapper"
-// import { taskGet } from "../actions/task-actions"
+import { taskGet } from "../actions/task-actions"
 
 export default async function Page() {
-  // const tasks = await taskGet()
+  const tasks = await taskGet()
 
-  // return <TaskCardWrapper tasks={tasks} />
-  return <TaskCardWrapper tasks={[]} />
+  return <TaskCardWrapper tasks={tasks} />
 }
