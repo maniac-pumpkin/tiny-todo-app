c3ee1011fe192cc97e3bf7f00b2969b276734f30
maniac-pumpkin <mohammadmehdibeheshti@yahoo.com>
2025-02-10 15:49:16 +0330

    refactor: comment out task fetching logic and return empty tasks array in dashboard page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,8 +1,9 @@
 import TaskCardWrapper from "@/components/task-card/task-card-wrapper"
-import { taskGet } from "../actions/task-actions"
+// import { taskGet } from "../actions/task-actions"
 
 export default async function Page() {
-  const tasks = await taskGet()
+  // const tasks = await taskGet()
 
-  return <TaskCardWrapper tasks={tasks} />
+  // return <TaskCardWrapper tasks={tasks} />
+  return <TaskCardWrapper tasks={[]} />
 }
