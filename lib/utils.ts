129b1c847ebf56ddfaeb7f987ae0b7ca4cdea8ee
maniac-pumpkin <mohammadmehdibeheshti@yahoo.com>
2025-02-10 15:50:57 +0330

    refactor: replace cookie-based token retrieval with client auth token in user username fetch

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,40 +1,20 @@
 import { clsx, type ClassValue } from "clsx"
 import { format } from "date-fns"
 import { twMerge } from "tailwind-merge"
+import { getClientAuthToken } from "./jwt-utils"
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
 
 export const formatDate = (date: Date | string) => format(date, "MMMM dd, yyyy")
-
-export const getCookie = (name: string) => {
-  const cookies = document.cookie.split("; ")
-
-  const cookie = cookies.find((c) => c.startsWith(`${name}=`))
-
-  if (cookie) {
-    const value = cookie.split("=")[1]
-
-    const expirationDate = new Date()
-    expirationDate.setHours(expirationDate.getHours() + 24)
-
-    document.cookie = `${name}=${value}; expires=${expirationDate.toUTCString()}; path=/`
-
-    return decodeURIComponent(value)
-  }
-
-  return null
-}
 
 export const deleteCookie = (name: string) => {
   document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`
 }
 
 export const userGetUsername = async () => {
-  const response = await fetch("/api/users", {
-    headers: {
-      authorization: `Bearer ${getCookie("token")}`,
-    },
-  })
+  const authorization = getClientAuthToken()!
+
+  const response = await fetch("/api/users", { headers: { authorization } })
 
   if (!response.ok) return ""
 
