edcbb9e0681bd1629abb239eec61c7bda5be4010
maniac-pumpkin <mohammadmehdibeheshti@yahoo.com>
2025-02-15 01:04:02 +0330

    refactor: enhance TaskCard styling with conditional classes for completion and deadline status

--- components/task-card/task-card-index.tsx
+++ components/task-card/task-card-index.tsx
@@ -2,7 +2,8 @@
 
 import { Badge } from "@/components/ui/badge"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { formatDate } from "@/lib/utils"
+import { cn, formatDate } from "@/lib/utils"
+import { isBefore, parseISO } from "date-fns"
 import { Calendar } from "lucide-react"
 import { memo } from "react"
 import TaskCardCompletedBtn from "./task-card-completed-btn"
@@ -22,15 +23,22 @@ export type TaskCardPropsType = {
 }
 
 function TaskCard(props: TaskCardPropsType) {
+  const isOutDated = isBefore(parseISO(props.deadline), new Date())
+  const isDone = props.isCompleted
+
   return (
     <Card className="flex flex-col justify-between">
       <CardHeader className="flex-row items-center justify-between space-y-0">
-        <CardTitle className="text-base">{props.title}</CardTitle>
+        <CardTitle className={cn("text-base", isDone && "line-through", isOutDated && "text-red-600")}>
+          {props.title}
+        </CardTitle>
         <Badge>{props.dirName}</Badge>
       </CardHeader>
       <CardContent className="space-y-5">
-        <CardDescription>{props.description}</CardDescription>
-        <p className="flex items-center gap-x-2 text-sm">
+        <CardDescription className={cn(isDone && "line-through", isOutDated && "text-red-600")}>
+          {props.description}
+        </CardDescription>
+        <p className={cn("flex items-center gap-x-2 text-sm", isDone && "line-through", isOutDated && "text-red-600")}>
           <Calendar size={16} />
           <span>{formatDate(props.deadline)}</span>
         </p>
