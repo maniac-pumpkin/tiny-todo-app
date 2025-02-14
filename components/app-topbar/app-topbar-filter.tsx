9b1f968b0791f0a9e5aa0e8d2a2343799a1b3bb3
maniac-pumpkin <mohammadmehdibeheshti@yahoo.com>
2025-02-15 02:03:42 +0330

    refactor: streamline filtering logic in AppTopbarFilter component using custom hook

--- components/app-topbar/app-topbar-filter.tsx
+++ components/app-topbar/app-topbar-filter.tsx
@@ -2,40 +2,24 @@
 
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
+import useFiltering from "@/hooks/use-filtering"
 import { LayoutList, List } from "lucide-react"
-import { usePathname, useRouter, useSearchParams } from "next/navigation"
-import { useEffect, useState } from "react"
 
 const selectOptions = ["Order added", "Earlier first", "Later first"]
 
 function AppTopbarFilter() {
-  const searchParams = useSearchParams()
-  const pathname = usePathname()
-  const router = useRouter()
-
-  const [renderMode, setRenderMode] = useState(searchParams.get("renderMode"))
-  const [sortBy, setSortBy] = useState(searchParams.get("sortBy"))
-
-  useEffect(() => {
-    const handler = setTimeout(() => {
-      const params = new URLSearchParams(searchParams)
-
-      if (renderMode) params.set("renderMode", renderMode)
-      if (sortBy) params.set("sortBy", sortBy)
-
-      router.replace(`${pathname}?${params.toString()}`)
-    }, 100)
-
-    return () => clearTimeout(handler)
-  }, [sortBy, renderMode, pathname, router, searchParams])
+  const renderMode = useFiltering((state) => state.renderMode)
+  const sortBy = useFiltering((state) => state.sortBy)
+  const updateRenderMode = useFiltering((state) => state.updateRenderMode)
+  const updateSortBy = useFiltering((state) => state.updateSortBy)
 
   return (
     <div className="flex justify-between">
       <ToggleGroup
-        className="hidden sm:block"
         type="single"
-        value={renderMode ?? "grid"}
-        onValueChange={(value) => value && setRenderMode(value)}
+        className="hidden sm:block"
+        value={renderMode}
+        onValueChange={(value: "grid" | "list") => value && updateRenderMode(value)}
       >
         <ToggleGroupItem value="grid">
           <LayoutList />
@@ -44,7 +28,7 @@ function AppTopbarFilter() {
           <List />
         </ToggleGroupItem>
       </ToggleGroup>
-      <Select value={sortBy ?? "Order added"} onValueChange={(value) => setSortBy(value)}>
+      <Select value={sortBy} onValueChange={updateSortBy}>
         <SelectTrigger className="w-full sm:w-44">
           <SelectValue placeholder="Sort by" />
         </SelectTrigger>
