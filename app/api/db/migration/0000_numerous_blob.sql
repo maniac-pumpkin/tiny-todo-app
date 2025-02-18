ca8e1a4212be9ce8a1772453bde2252ec9e5cdd0
maniac-pumpkin <mohammadmehdibeheshti@yahoo.com>
2025-02-18 22:58:11 +0330

    refactor: remove obsolete migration files and update schema for directories and tasks

--- app/api/db/migration/0000_numerous_blob.sql
+++ app/api/db/migration/0000_numerous_blob.sql
@@ -7,8 +7,8 @@ CREATE TABLE "directories" (
 --> statement-breakpoint
 CREATE TABLE "tasks" (
 	"id" serial PRIMARY KEY NOT NULL,
-	"title" varchar(25) NOT NULL,
-	"description" varchar(80) DEFAULT '',
+	"title" varchar(35) NOT NULL,
+	"description" varchar(100) DEFAULT '',
 	"deadline" timestamp NOT NULL,
 	"is_important" boolean DEFAULT false,
 	"is_completed" boolean DEFAULT false,
@@ -20,18 +20,8 @@ CREATE TABLE "users" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"username" varchar(100) NOT NULL,
 	"password" text NOT NULL,
-	"email" varchar(254) NOT NULL,
-	"is_verified" boolean DEFAULT false,
-	CONSTRAINT "users_username_unique" UNIQUE("username"),
-	CONSTRAINT "users_email_unique" UNIQUE("email")
-);
---> statement-breakpoint
-CREATE TABLE "verify_token" (
-	"user_id" integer NOT NULL,
-	"token" text NOT NULL,
-	"exp_date" timestamp DEFAULT NOW() + INTERVAL '10 minutes' NOT NULL
+	CONSTRAINT "users_username_unique" UNIQUE("username")
 );
 --> statement-breakpoint
 ALTER TABLE "directories" ADD CONSTRAINT "fk_user_directories" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
-ALTER TABLE "tasks" ADD CONSTRAINT "tasks_directory_id_directories_id_fk" FOREIGN KEY ("directory_id") REFERENCES "public"."directories"("id") ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
-ALTER TABLE "verify_token" ADD CONSTRAINT "verify_token_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+ALTER TABLE "tasks" ADD CONSTRAINT "tasks_directory_id_directories_id_fk" FOREIGN KEY ("directory_id") REFERENCES "public"."directories"("id") ON DELETE cascade ON UPDATE cascade;
--- app/api/db/migration/0002_melted_veda.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE "verify_token" CASCADE;--> statement-breakpoint
-ALTER TABLE "users" DROP COLUMN "is_verified";
