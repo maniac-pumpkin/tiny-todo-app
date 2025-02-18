9b28f951ff40151f52e751335aa714541110d58a
maniac-pumpkin <mohammadmehdibeheshti@yahoo.com>
2025-02-18 22:57:58 +0330

    refactor: update drizzle initialization to use an object for the database client

--- app/api/db/index.ts
+++ app/api/db/index.ts
@@ -2,6 +2,6 @@ import { neon } from "@neondatabase/serverless"
 import { drizzle } from "drizzle-orm/neon-http"
 
 const sql = neon(process.env["POSTGRES_URL"]!)
-const db = drizzle(sql)
+const db = drizzle({ client: sql })
 
 export default db
